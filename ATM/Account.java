4372ae6e2e33333de72df5c39fa9e6a19407550f
nimit-kodrani <nimit.kodrani@gmail.com>
2022-10-20 21:43:56 +0530

    Withdrawal logic

--- ATM/Account.java
+++ ATM/Account.java
@@ -85,7 +85,24 @@ public class Account {
 
 	public void getCheckingWithdrawInput() {
 		
-		//yet to be implemented
+		boolean end = false;
+		while (!end) {
+			try {
+				System.out.println("\nCurrent Checkings Account Balance: " + moneyFormat.format(checkingBalance));
+				System.out.print("\nAmount you want to withdraw from Checkings Account: ");
+				double amount = input.nextDouble();
+				if ((checkingBalance - amount) >= 0 && amount >= 0) {
+					calcCheckingWithdraw(amount);
+					System.out.println("\nCurrent Checkings Account Balance: " + moneyFormat.format(checkingBalance));
+					end = true;
+				} else {
+					System.out.println("\nBalance Cannot be Negative.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				input.next();
+			}
+		}
 	}
 
 	public void getsavingWithdrawInput() {
