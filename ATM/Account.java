81a33b1424a6c5624c4390326da2aaaf15cb823d
nimit-kodrani <nimit.kodrani@gmail.com>
2022-10-21 11:07:24 +0530

    transfer logic
    transfer logic

--- ATM/Account.java
+++ ATM/Account.java
@@ -139,6 +139,67 @@ public class Account {
 
 	public void getTransferInput(String accType) {
 		
-		//yet to be implemented
+		boolean end = false;
+		while (!end) {
+			try {
+				if (accType.equals("Checkings")) {
+					System.out.println("\nSelect an account you wish to tranfers funds to:");
+					System.out.println("1. Savings");
+					System.out.println("2. Exit");
+					System.out.print("\nChoice: ");
+					int choice = input.nextInt();
+					switch (choice) {
+					case 1:
+						System.out.println("\nCurrent Checkings Account Balance: " + moneyFormat.format(checkingBalance));
+						System.out.print("\nAmount you want to deposit into your Savings Account: ");
+						double amount = input.nextDouble();
+						if ((savingBalance + amount) >= 0 && (checkingBalance - amount) >= 0 && amount >= 0) {
+							calcCheckTransfer(amount);
+							System.out.println("\nCurrent Savings Account Balance: " + moneyFormat.format(savingBalance));
+							System.out.println(
+									"\nCurrent Checkings Account Balance: " + moneyFormat.format(checkingBalance));
+							end = true;
+						} else {
+							System.out.println("\nBalance Cannot Be Negative.");
+						}
+						break;
+					case 2:
+						return;
+					default:
+						System.out.println("\nInvalid Choice.");
+						break;
+					}
+				} else if (accType.equals("Savings")) {
+					System.out.println("\nSelect an account you wish to tranfers funds to: ");
+					System.out.println("1. Checkings");
+					System.out.println("2. Exit");
+					System.out.print("\nChoice: ");
+					int choice = input.nextInt();
+					switch (choice) {
+					case 1:
+						System.out.println("\nCurrent Savings Account Balance: " + moneyFormat.format(savingBalance));
+						System.out.print("\nAmount you want to deposit into your savings account: ");
+						double amount = input.nextDouble();
+						if ((checkingBalance + amount) >= 0 && (savingBalance - amount) >= 0 && amount >= 0) {
+							calcSavingTransfer(amount);
+							System.out.println("\nCurrent checkings account balance: " + moneyFormat.format(checkingBalance));
+							System.out.println("\nCurrent savings account balance: " + moneyFormat.format(savingBalance));
+							end = true;
+						} else {
+							System.out.println("\nBalance Cannot Be Negative.");
+						}
+						break;
+					case 2:
+						return;
+					default:
+						System.out.println("\nInvalid Choice.");
+						break;
+					}
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nInvalid Choice.");
+				input.next();
+			}
+		}
 	}
 }
